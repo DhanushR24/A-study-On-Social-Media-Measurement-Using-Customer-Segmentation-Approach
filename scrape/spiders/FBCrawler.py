99d85d0202d235fdc59e80cf1932c1c9f83fbe7a
Dhanush Raj <dhanushra66@gmail.com>
2022-09-15 19:55:36 +0530

    Scrape FB no. of users data :sparkles:

---
+++ scrape/spiders/FBCrawler.py
@@ -0,0 +1,52 @@
+import scrapy
+import re
+from scrapy_selenium import SeleniumRequest
+import pandas as pd
+
+from .pdf import getPDFContent
+
+contentLst = list()
+
+class MetaSpider(scrapy.Spider):
+    name = 'FBCrawler'
+
+    def start_requests(self):
+        urls = ["https://investor.fb.com/investor-events/default.aspx"]
+
+        for url in urls:
+            yield SeleniumRequest(
+                url         = url,
+                wait_time   = 3,
+                screenshot  = True,
+                callback    = self.parse,
+                dont_filter = True
+            )
+
+    def parse(self, response):
+        # page = response.url.split("/")[-2]
+        # filename = f"content-{page}.html"
+
+        # with open(filename, "wb") as f:
+        #     f.write(response.body)
+        #     self.log(f"Saved file {filename}")
+
+        container = response.xpath('//*[@id="_ctrl0_ctl54_divModuleContainer"]/div[1]/div').css(".ModuleItemRow")
+        
+        for item in container:
+            title = item.css('.ModuleHeadlineLink').xpath('text()').get()
+            
+            if "Earnings" not in title:
+                continue
+
+            title   = re.sub(r'\s+', ' ', title)
+            pdf     = item.css('.RelatedDocuments div:first-child a::attr(href)').get()
+            content = getPDFContent(pdf)
+
+            print(f"{title:<20} : {content}")
+
+            contentLst.append({'quarter':title, 'DAP': content[0], 'MAP': content[1], 'DAU': content[2], 'MAU': content[3]})
+
+        contents = pd.DataFrame.from_records(contentLst, columns=['quarter', 'DAP', 'MAP', 'DAU', 'MAU'])
+        contents.to_csv("FB.csv", index=False)
+
+
